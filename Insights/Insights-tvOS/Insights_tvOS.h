7b8758c559c4b2e56cd6c893b6831f768877d592
Robert Mogos <robert.mogos@algolia.com>
2018-06-12 11:36:26 +0200

    Watch + TV targets

---
+++ Insights/Insights-tvOS/Insights_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Insights_tvOS.h
+//  Insights-tvOS
+//
+//  Created by Robert Mogos on 12/06/2018.
+//  Copyright © 2018 Algolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Insights_tvOS.
+FOUNDATION_EXPORT double Insights_tvOSVersionNumber;
+
+//! Project version string for Insights_tvOS.
+FOUNDATION_EXPORT const unsigned char Insights_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Insights_tvOS/PublicHeader.h>
+
+
