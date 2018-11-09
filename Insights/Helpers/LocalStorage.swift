c89823241856f29ffbe95a4749ec388760c04590
Vladislav Fitc <vladislav.fitc@algolia.com>
2018-11-09 17:42:40 +0100

    Added convenient delete function to local storage

--- Insights/Helpers/LocalStorage.swift
+++ Insights/Helpers/LocalStorage.swift
@@ -9,40 +9,53 @@
 import Foundation
 
 class LocalStorage<V: Codable> {
-  static func filePath(for index: String) -> String? {
-    let filename = "algolia-\(index)"
-    let manager = FileManager.default
+    static func filePath(for index: String) -> String? {
+        let filename = "algolia-\(index)"
+        let manager = FileManager.default
+        
+        #if os(iOS)
+        let url = manager.urls(for: .libraryDirectory, in: .userDomainMask).last
+        #else
+        let url = manager.urls(for: .cachesDirectory, in: .userDomainMask).last
+        #endif // os(iOS)
+        
+        guard let urlUnwrapped = url?.appendingPathComponent(filename).path else {
+            return nil
+        }
+        return urlUnwrapped
+    }
     
-    #if os(iOS)
-    let url = manager.urls(for: .libraryDirectory, in: .userDomainMask).last
-    #else
-    let url = manager.urls(for: .cachesDirectory, in: .userDomainMask).last
-    #endif // os(iOS)
+    @discardableResult static func deleteFile(atPath path: String) -> Bool {
+        let exists = FileManager.default.fileExists(atPath: path)
+        if let url = URL(string: path), exists {
+            do {
+                try FileManager.default.removeItem(at: url)
+            } catch let error {
+                debugPrint(error.localizedDescription)
+                return false
+            }
+        }
+        return exists
+    }
     
-    guard let urlUnwrapped = url?.appendingPathComponent(filename).path else {
-      return nil
+    @discardableResult static func serialize(_ objects: V, file: String) -> Bool {
+        do {
+            let data = try PropertyListEncoder().encode(objects)
+            return NSKeyedArchiver.archiveRootObject(data, toFile: file)
+        } catch {
+            return false
+        }
     }
-    return urlUnwrapped
-  }
-  
-  @discardableResult static func serialize(_ objects: V, file: String) -> Bool {
-    do {
-      let data = try PropertyListEncoder().encode(objects)
-      return NSKeyedArchiver.archiveRootObject(data, toFile: file)
-    } catch {
-      return false
+    
+    static func deserialize(_ file: String) -> V? {
+        guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: file) as? Data else {
+            return nil
+        }
+        do {
+            let object = try PropertyListDecoder().decode(V.self, from: data)
+            return object
+        } catch {
+            return nil
+        }
     }
-  }
-  
-  static func deserialize(_ file: String) -> V? {
-    guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: file) as? Data else {
-      return nil
-    }
-    do {
-      let object = try PropertyListDecoder().decode(V.self, from: data)
-      return object
-    } catch {
-      return nil
-    }
-  }
 }
