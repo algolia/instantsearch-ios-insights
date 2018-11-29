fdf4df5e7b35da1a995158831bb905317f79b97d
Vladislav Fitc <vladislav.fitc@algolia.com>
2018-11-29 11:54:53 +0100

    Unit tests for encoding/decoding of ObjectIDsOrFilters

--- InsightsTests/Unit/ObjectIDsOrFiltersTests.swift
+++ InsightsTests/Unit/ObjectIDsOrFiltersTests.swift
@@ -11,12 +11,43 @@ import XCTest
 
 class ObjectIDsOrFiltersTests: XCTestCase {
     
-    func testEncoding() {
-        
+    func testObjectIDsEncoding() {
+        let rawObjectIDs = ["o1", "o2", "o3"]
+        let objectIDs = ObjectsIDsOrFilters.objectIDs(rawObjectIDs)
+        let serializedObjectIDs = Dictionary(objectIDs)!
+        let expectedSerializedObjectIDs: Dictionary<String, Any> = [ObjectsIDsOrFilters.CodingKeys.objectIDs.rawValue: rawObjectIDs]
+        XCTAssertTrue(NSDictionary(dictionary: serializedObjectIDs).isEqual(to: expectedSerializedObjectIDs))
     }
     
-    func testDecoding() {
-        
+    func testFiltersEncoding() {
+        let rawFilters = ["f1", "f2", "f3"]
+        let filters = ObjectsIDsOrFilters.filters(rawFilters)
+        let serializedFilters = Dictionary(filters)!
+        let expectedSerializedFilters: Dictionary<String, Any> = [ObjectsIDsOrFilters.CodingKeys.filters.rawValue: rawFilters]
+        XCTAssertTrue(NSDictionary(dictionary: serializedFilters).isEqual(to: expectedSerializedFilters))
+    }
+    
+    func testObjectIDsDecoding() {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: [ObjectsIDsOrFilters.CodingKeys.objectIDs.rawValue: ["o1", "o2", "o3"]], options: [])
+            let objectIDs = try JSONDecoder().decode(ObjectsIDsOrFilters.self, from: jsonData)
+            let expectedObjectIDs = ObjectsIDsOrFilters.objectIDs(["o1", "o2", "o3"])
+            XCTAssertEqual(objectIDs, expectedObjectIDs)
+        } catch let error {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func testFiltersDecoding() {
+        let rawFilters = ["f1", "f2", "f3"]
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: [ObjectsIDsOrFilters.CodingKeys.filters.rawValue: rawFilters], options: [])
+            let filters = try JSONDecoder().decode(ObjectsIDsOrFilters.self, from: jsonData)
+            let expectedFilters = ObjectsIDsOrFilters.filters(rawFilters)
+            XCTAssertEqual(filters, expectedFilters)
+        } catch let error {
+            XCTFail("\(error)")
+        }
     }
     
     func testFailedDecoding() {
