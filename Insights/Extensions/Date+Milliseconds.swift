546901c4289eb1dacea873906ac3cbd4b3ebcaf4
Vladislav Fitc <vladislav.fitc@algolia.com>
2018-12-04 14:06:34 +0100

    Added Date+milliseconds to all the targets

--- Insights/Extensions/Date+Milliseconds.swift
+++ Insights/Extensions/Date+Milliseconds.swift
@@ -12,7 +12,6 @@ public extension Date {
     
     var millisecondsSince1970: Int64 {
         return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
-        //RESOLVED CRASH HERE
     }
     
     init(milliseconds: Int) {
