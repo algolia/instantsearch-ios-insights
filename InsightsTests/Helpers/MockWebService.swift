4cdf707a5468a2354c5ab4564287c2e9b6a4c109
Guy Daher <guy.daher@algolia.com>
2019-03-27 11:54:30 +0100

    fix tests

--- InsightsTests/Helpers/MockWebService.swift
+++ InsightsTests/Helpers/MockWebService.swift
@@ -17,7 +17,7 @@ class MockWebService: WebService {
     super.init(sessionConfig: sessionConfig, logger: logger)
   }
   
-  public override func load<A, E>(resource: Resource<A, E>, completion: @escaping (Result<A, E>) -> ()) {
+  public override func load<A, E>(resource: Resource<A, E>, completion: @escaping (Result<A?, Error>) -> ()) {
     stub(resource)
   }
   
